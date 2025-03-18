dd44d76bf358e183fa739c42be1f52d8a14c7067
abhilash111 <abhilash.k@globalsinc.com>
2025-03-18 22:58:56 +0530

    updated password

--- config/env.go
+++ config/env.go
@@ -27,7 +27,7 @@ func initConfig() Config {
 		PublicHost:             getEnv("PUBLIC_HOST", "http://localhost"),
 		Port:                   getEnv("PORT", "8080"),
 		DBUser:                 getEnv("DB_USER", "root"),
-		DBPassword:             getEnv("DB_PASSWORD", "password"),
+		DBPassword:             getEnv("DB_PASSWORD", "kulkarni11"), // Also password
 		DBAddress:              fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
 		DBName:                 getEnv("DB_NAME", "ecom"),
 		JWTExpirationInSeconds: getEnvAsInt("JWT_EXPIRATION_IN_SECONDS", 3600*24*7),
