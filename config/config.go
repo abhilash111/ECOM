cd62736fde8b7ef1352d228f7d668374510f3c9a
abhilash111 <abhilashkulkarni11@gmail.com>
2025-07-05 11:41:33 +0530

    updated redis

--- config/config.go
+++ config/config.go
@@ -17,6 +17,12 @@ type Config struct {
 	DBName                 string
 	JWTExpirationInSeconds int64
 	JWTSecret              string
+	RedisHost              string
+	RedisPort              string
+	CognitoRegion          string
+	CognitoPoolID          string
+	CognitoAppID           string
+	LambdaOTPARN           string
 }
 
 var Envs = initConfig()
@@ -28,19 +34,26 @@ func initConfig() Config {
 		Port:       getEnv("PORT", "8080"),
 		DBUser:     getEnv("DB_USER", "root"),
 		DBPassword: getEnv("DB_PASSWORD", "kulkarni11"), // Also password_
-		DBAddress:  fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
-		// DBAddress:  fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3307")), use this for running locally
+		// DBAddress:  fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
+		DBAddress:              fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3307")), // use this for running locally
 		DBName:                 getEnv("DB_NAME", "ecom"),
 		JWTExpirationInSeconds: getEnvAsInt("JWT_EXPIRATION_IN_SECONDS", 3600*24*7),
 		JWTSecret:              getEnv("JWT_SECRET", "not-secret-anymore"),
+		RedisHost:              getEnv("REDIS_HOST", "localhost"),
+		RedisPort:              getEnv("REDIS_PORT", "6379"),
+		CognitoRegion:          getEnv("COGNITO_REGION", "ap-south-1"),
+		CognitoPoolID:          getEnv("COGNITO_POOL_ID", ""),
+		CognitoAppID:           getEnv("COGNITO_APP_ID", ""),
+		LambdaOTPARN:           getEnv("LAMBDA_OTP_ARN", ""),
 	}
 }
 
-func getEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-		return value
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
 	}
-	return fallback
+	return value
 }
 
 func getEnvAsInt(key string, fallback int64) int64 {
